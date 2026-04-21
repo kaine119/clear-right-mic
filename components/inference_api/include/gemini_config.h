017d4e2a9e6983611f348f96f61e6799c8e51e1e
Mui Kai En <9492636+kaine119@users.noreply.github.com>
2026-04-21 13:44:35 +0800

    Check in revised Gemini config

--- components/inference_api/include/gemini_config.h
+++ components/inference_api/include/gemini_config.h
@@ -3,9 +3,12 @@
 
 // DO NOT CHECK IN THE FOLLOWING LINE
 #define GEMINI_API_KEY ""
-#define GEMINI_URL "https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent"
 
-#define TEST_PROMPT "A Gold code, also known as Gold sequence, is a type of binary sequence used in telecommunications (CDMA)[1] and satellite navigation (GPS).[2] Gold codes are named after Robert Gold.[3][4] Gold codes have bounded small cross-correlations within a set, which is useful when multiple devices are broadcasting in the same frequency range. A set of Gold code sequences consists of 2n + 1 sequences each one with a period of 2n − 1. "
-#define SYSTEM_INSTRUCTION "You are a first-year university engineering student. Does the following make sense to you? Answer yes or no only, do not give any additional context."
+#define GEMINI_BASE_URL "https://generativelanguage.googleapis.com"
+#define GEMINI_URL GEMINI_BASE_URL "/v1beta/models/gemini-3.1-flash-lite-preview:generateContent"
+#define GEMINI_FILE_INITIATE_URL GEMINI_BASE_URL "/upload/v1beta/files"
+
+
+#define SYSTEM_INSTRUCTION "You are a first-year university engineering student. Transcribe the following recordings, and tell me if they make sense to you from a student's perspective."
 
 #endif
